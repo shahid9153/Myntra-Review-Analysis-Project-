c1ef13b43327de20e41c847a41f42660d520de01
shahid mulani <145219577+shahid9153@users.noreply.github.com>
2024-12-02 00:25:57 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Review analyzer',
+    version= '0.0.1',
+    author='shahid mulani', 
+    author_email='shahidkmulani@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
